5e6d0d1077bef60d83cf61671bf7e261500e299f
Mark Wubben <mark@novemberborn.net>
2011-05-02 18:58:21 +0100

    Remove test for dojo.config.deferredOnError, no longer used

--- tests/_base/Deferred.js
+++ tests/_base/Deferred.js
@@ -146,15 +146,6 @@ doh.register("tests._base.Deferred",
 			}
 			return null;
 		},
-		function errorHandler(t){
-			var def = new dojo.Deferred();
-			var handledError;
-			dojo.config.deferredOnError = function(e){
-				handledError = e;
-			};
-			def.reject(new Error("test"));
-			t.t(handledError instanceof Error);
-		},
 		function cancelThenDerivative(t){
 			var def = new dojo.Deferred();
 			var def2 = def.then();
